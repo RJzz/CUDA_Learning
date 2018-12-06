4762c5ea7ecd3eabb021252f7d0448f17218e254
RJzz <jieruiruir@gmail.com>
2018-12-06 17:11:00 +0800

    aaa

--- hello_cuda.cu
+++ hello_cuda.cu
@@ -10,7 +10,7 @@ int main() {
         cudaGetDeviceProperties(&devProp, i);
         std::cout << "using GPU device" << i << ":" << devProp.name << std::endl;
         std::cout << "设备全局内存总量:   " << devProp.totalGlobalMem / 1024 / 1024 << "MB" << std::endl;
-        std::cout << "SM的数量: << devProp.multiProcessorCount << std::endl;
+        std::cout << "SM的数量:" << devProp.multiProcessorCount << std::endl;
         std::cout << "每个线程块的共享内存大小: " << devProp.sharedMemPerBlock / 1024.0 << "KB" << std::endl;
         std::cout << "每个线程块的最大线程数：" << devProp.maxThreadPerBlock << std::endl;
         std::cout << "设备上一个线程块（Block）中可用的32位寄存器的数量: " << devProp.regsPerBlock << std::endl;
